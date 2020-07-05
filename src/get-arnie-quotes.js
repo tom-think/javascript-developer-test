a7a58c1a7d0d21e3601d62cfc9deacccb5e77e92
Thomas McGuirk <thomasmcguirk@Thomass-MacBook-Pro.local>
2020-07-05 21:51:31 +1000

    Finished test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const calls = urls.map(url => httpGet(url));
+
+  const responses = await Promise.all(calls);
+
+  return responses.map(res => {
+    if (res.status === 200) {
+      return { 'Arnie Quote': JSON.parse(res.body).message }
+    }
+    return { FAILURE: 'Your request has been terminated' }
+  });
 };
 
 module.exports = {
